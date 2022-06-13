a06363e248e8a418b6b2a75b98bb9b75f3f12521
Juan G Lopez <nahujg@gmail.com>
2022-06-13 13:14:22 +0200

    Splash screen añadida
    canvas oculto

--- LaberintoJuego/src/index.js
+++ LaberintoJuego/src/index.js
@@ -4,11 +4,14 @@ const ctx = canvas.getContext("2d");
 
 
 
-canvas.style.backgroundColor = "green";
 
+
+
+//imagen cabeza
 let cabezaImagen = new Image();
 cabezaImagen.src = "img/cabeza2.png";
 
+//imagenes muros exteriores
 let pared450x30Imagen = new Image();
 pared450x30Imagen.src = "img/pared450x30.png";
 
@@ -31,7 +34,6 @@ let pared690x30Imagen = new Image();
 pared690x30Imagen.src = "img/pared690x30.png";
 
 //imagenes muros anchos
-
 let pared110x20Imagen = new Image();
 pared110x20Imagen.src = "img/paredesanchas/pared110x20.png";
 
@@ -68,65 +70,38 @@ let gameOver = false
 
 let limpiarInterval; 
 
+//Dibujando imágenes en el canvas
+//cabeza
 const cabeza = new Objeto(380, 650, 40, 40, cabezaImagen, ctx);
-
+//muros exteriores
 const pared1 = new Objeto (0, 0, 1140, 30, pared1140x30Imagen, ctx);
-
 const pared2 = new Objeto (0, 30, 30, 640, pared30x640Imagen, ctx);
-
 const pared3 = new Objeto (1110, 30, 30, 110, pared30x110Imagen, ctx);
-
 const pared4 = new Objeto (1110, 230, 30, 440, pared30x440Imagen, ctx);
-
 const pared5 = new Objeto (0, 670, 360, 30, pared360x30Imagen, ctx);
-
 const pared6 = new Objeto (450, 670, 690, 30, pared690x30Imagen, ctx);
-
-//crear muros anchos
-
+//muros anchos
 const pared7 = new Objeto (120, 120, 900, 20, pared900x20Imagen, ctx);
-
 const pared8 = new Objeto (30, 230, 110, 20, pared110x20Imagen, ctx);
-
 const pared9 = new Objeto (340, 230, 350, 20, pared350x20Imagen, ctx);
-
 const pared10 = new Objeto (890, 230, 220, 20, pared220x20Imagen, ctx);
-
 const pared11 = new Objeto (120, 340, 130, 20, pared130x20Imagen, ctx);
-
 const pared12 = new Objeto (340, 340, 130, 20, pared130x20Imagen, ctx);
-
 const pared13 = new Objeto (670, 340, 240, 20, pared240x20Imagen, ctx);
-
 const pared14 = new Objeto (120, 450, 900, 20, pared900x20Imagen, ctx);
-
 const pared15 = new Objeto (230, 560, 350, 20, pared350x20Imagen, ctx);
-
 const pared16 = new Objeto (780, 560, 240, 20, pared240x20Imagen, ctx);
-
-//crear muros altos
-
+//muros altos
 const pared17 = new Objeto (120, 470, 20, 200, pared20x200Imagen, ctx);
-
 const pared18 = new Objeto (230, 140, 20, 200, pared20x200Imagen, ctx);
-
 const pared19 = new Objeto (340, 250, 20, 90, pared20x90Imagen, ctx);
-
 const pared20 = new Objeto (450, 580, 20, 90, pared20x90Imagen, ctx);
-
 const pared21 = new Objeto (560, 250, 20, 200, pared20x200Imagen, ctx);
-
 const pared22 = new Objeto (670, 470, 20, 130, pared20x130Imagen, ctx);
-
 const pared23 = new Objeto (780, 580, 20, 90, pared20x90Imagen, ctx);
-
 const pared24 = new Objeto (780, 140, 20, 200, pared20x200Imagen, ctx);
-
 const pared25 = new Objeto (890, 250, 20, 90, pared20x90Imagen, ctx);
-
 const pared26 = new Objeto (1000, 340, 20, 110, pared20x110Imagen, ctx);
-
-
 
 
 const jugar = () => {
