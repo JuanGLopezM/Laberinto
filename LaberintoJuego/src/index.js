752ea723a6c5157a52eca3acb293b05308dfa9c4
Juan G Lopez <nahujg@gmail.com>
2022-06-14 16:30:23 +0200

    limpiando un poco

--- LaberintoJuego/src/index.js
+++ LaberintoJuego/src/index.js
@@ -1,4 +1,4 @@
-//variables globales
+// Pantallas
 let splashScreen = document.querySelector("#splash-screen");
 let gameOverScreen = document.querySelector("#gameover-screen");
 let winGameScreen = document.querySelector("#wingame-screen");
@@ -6,10 +6,7 @@ let howToPlayScreen = document.querySelector("#howtoplay-screen");
 let soundSettingScreen = document.querySelector("#soundsetting-screen");
 const canvas = document.querySelector("#canvas");
 
-//const audio = document.getElementById(".....");
-
-
-//botones (restart comentados porque no reincian las colisiones)
+// Botones
 let startButton = document.querySelector("#start-btn");
 let howToPlayButton = document.querySelector("#how-to-play");
 let soundSettingButton = document.querySelector("#sound-setting");
@@ -20,14 +17,19 @@ const goToMenu3 = document.getElementById("back-to-menu3");
 const goToMenu4 = document.getElementById("back-to-menu4");
 const soundOn = document.getElementById("sound-on");
 const soundOff = document.getElementById("sound-off");
-//const restartGame = document.getElementById("restart-btn");
-//const restartGame2 = document.getElementById("restart-btn2");
-//ctx
+
+// Sonidos
+// const audio = document.getElementById(".....");
+// const audio = document.getElementById(".....");
+// ...
+
+// ctx
 const ctx = canvas.getContext("2d");
-// juego está activo o no
+
+// Juego activo= sí o no (variable para startGame)
 let checkIfGameIsRunning = false;
 
-//Inicio juego-->mostrar canvas
+// Pantalla Jugar-->mostrar canvas
 const startGame = () => {
     checkIfGameIsRunning = true;
     if (checkIfGameIsRunning) {
@@ -35,22 +37,24 @@ const startGame = () => {
       canvas.style.display = "flex";
     }
 };
-//Pantalla Cómo jugar
+// Pantalla Cómo jugar
 const startHowToPlay = () => {
     splashScreen.style.display = "none";
     howToPlayScreen.style.display = "flex"
 }
+
+// Pantalla Sonido
 const startSoundSetting = () => {
     splashScreen.style.display = "none";
     soundSettingScreen.style.display = "flex"
 }
 
 
-//imagen cabeza
+// Imagen cabeza
 let cabezaImagen = new Image();
 cabezaImagen.src = "img/cabeza.png";
 
-//imagenes muros exteriores
+// Imágenes muros exteriores
 let pared1140x30Imagen = new Image();
 pared1140x30Imagen.src = "img/paredesexternas/pared1140x30.png";
 
@@ -69,7 +73,7 @@ pared360x30Imagen.src = "img/paredesexternas/pared360x30.png";
 let pared690x30Imagen = new Image();
 pared690x30Imagen.src = "img/paredesexternas/pared690x30.png";
 
-//imagenes muros anchos
+// Imágenes muros anchos
 let pared110x20Imagen = new Image();
 pared110x20Imagen.src = "img/paredesanchas/pared110x20.png";
 
@@ -88,7 +92,7 @@ pared350x20Imagen.src = "img/paredesanchas/pared350x20.png";
 let pared900x20Imagen = new Image();
 pared900x20Imagen.src = "img/paredesanchas/pared900x20.png";
 
-//imagenes muros altos
+// Imágenes muros altos
 
 let pared20x90Imagen = new Image();
 pared20x90Imagen.src = "img/paredesaltas/pared20x90.png";
@@ -102,26 +106,26 @@ pared20x130Imagen.src = "img/paredesaltas/pared20x130.png";
 let pared20x200Imagen = new Image();
 pared20x200Imagen.src = "img/paredesaltas/pared20x200.png";
 
-//imágen pinchos
+// Imagen pinchos
 
 let pinchosImagen = new Image();
 pinchosImagen.src = "img/pinchos.png";
 
-//variable para retear intervalo
+// Variable para parar intervalo ()
 
 let limpiarInterval; 
 
-//Dibujando imágenes en el canvas
-//cabeza
+// Dibujando imágenes en el canvas
+// Cabeza
 const cabeza = new Objeto(380, 650, 40, 40, cabezaImagen, ctx);
-//muros exteriores
+// Muros exteriores
 const pared1 = new Objeto (0, 0, 1140, 30, pared1140x30Imagen, ctx);
 const pared2 = new Objeto (0, 30, 30, 640, pared30x640Imagen, ctx);
 const pared3 = new Objeto (1110, 30, 30, 110, pared30x110Imagen, ctx);
 const pared4 = new Objeto (1110, 230, 30, 440, pared30x440Imagen, ctx);
 const pared5 = new Objeto (0, 670, 360, 30, pared360x30Imagen, ctx);
 const pared6 = new Objeto (450, 670, 690, 30, pared690x30Imagen, ctx);
-//muros anchos
+// Muros anchos
 const pared7 = new Objeto (120, 120, 900, 20, pared900x20Imagen, ctx);
 const pared8 = new Objeto (30, 230, 110, 20, pared110x20Imagen, ctx);
 const pared9 = new Objeto (340, 230, 350, 20, pared350x20Imagen, ctx);
@@ -132,7 +136,7 @@ const pared13 = new Objeto (670, 340, 240, 20, pared240x20Imagen, ctx);
 const pared14 = new Objeto (120, 450, 900, 20, pared900x20Imagen, ctx);
 const pared15 = new Objeto (230, 560, 350, 20, pared350x20Imagen, ctx);
 const pared16 = new Objeto (780, 560, 240, 20, pared240x20Imagen, ctx);
-//muros altos
+// Muros altos
 const pared17 = new Objeto (120, 470, 20, 200, pared20x200Imagen, ctx);
 const pared18 = new Objeto (230, 140, 20, 200, pared20x200Imagen, ctx);
 const pared19 = new Objeto (340, 250, 20, 90, pared20x90Imagen, ctx);
@@ -143,16 +147,16 @@ const pared23 = new Objeto (780, 580, 20, 90, pared20x90Imagen, ctx);
 const pared24 = new Objeto (780, 140, 20, 200, pared20x200Imagen, ctx);
 const pared25 = new Objeto (890, 250, 20, 90, pared20x90Imagen, ctx);
 const pared26 = new Objeto (1000, 340, 20, 110, pared20x110Imagen, ctx);
-//pinchos
+// Pinchos
 const pinchos1 = new Objeto (150, 590, 100, 70, pinchosImagen, ctx);
 const pinchos2 = new Objeto (700, 480, 100, 70, pinchosImagen, ctx);
 const pinchos3 = new Objeto (590, 370, 100, 70, pinchosImagen, ctx);
 const pinchos4 = new Objeto (470, 150, 100, 70, pinchosImagen, ctx);
-const pinchos5 = new Objeto (100, 370, 100, 70, pinchosImagen, ctx);
-const pinchos6 = new Objeto (970, 40, 100, 70, pinchosImagen, ctx);
+const pinchos5 = new Objeto (40, 370, 100, 70, pinchosImagen, ctx);
+const pinchos6 = new Objeto (1000, 40, 100, 70, pinchosImagen, ctx);
 let estanPinchosActivos = false
 
-//cargando imagenes e iniciando interval
+// Cargando imágenes e iniciando intervalo
 const cargaInicial = () => {
     cabeza.dibujar();
     pared1.dibujar();
@@ -185,7 +189,7 @@ const cargaInicial = () => {
     limpiarInterval = setInterval(colisiones, 10);
 };
 
-//Parpadeo pinchos
+// Parpadeo pinchos
 alternar =()=> {
     if (estanPinchosActivos === true){
         console.log("entra")
@@ -207,10 +211,10 @@ alternar =()=> {
     }
 }
 function intervaloPinchos () {
-    setInterval(alternar, 3000)
+    setInterval(alternar, 3200)
 }
 
-//Detectando colisiones-->gameOver, incluye win
+// Detectando colisiones-->gameOver, incluye win
 const colisiones = () => {
     if (cabeza.detectarColision(pared1)) {
         canvas.style.display = "none";
@@ -347,33 +351,33 @@ const colisiones = () => {
     } 
 }
 
-//movimiento cabeza
+// Movimiento cabeza
 const moverCabeza = (e) => {
     cabeza.borrar();
     if (e.key === "ArrowLeft") {
         if (cabeza.x > 0) {
-            cabeza.x -= 5;
+            cabeza.x -= 10;
       }
     }
     if (e.key === "ArrowRight") {
         if (cabeza.x < 1100) {
-            cabeza.x += 5;
+            cabeza.x += 10;
       }
     }
     if (e.key === "ArrowUp") {
         if (cabeza.y > 0) {
-            cabeza.y -= 5;}
+            cabeza.y -= 10;}
     }
     if (e.key === "ArrowDown") {
         if (cabeza.y < 660) {
-            cabeza.y += 5;
+            cabeza.y += 10;
         }
-
-    e.preventDefault();}  //previene que se mueva la pantalla con las teclas de flechas
-
+    }
+    e.preventDefault();  //previene que se mueva la pantalla con las teclas de las flechas
     cabeza.dibujar();
 };
 
+// Listeners
 startButton.addEventListener("click", startGame);
 howToPlayButton.addEventListener("click", startHowToPlay);
 soundSettingButton.addEventListener("click", startSoundSetting);
@@ -391,16 +395,6 @@ goToMenu3.addEventListener("click", () => {
 goToMenu4.addEventListener("click", () => {
     window.location.reload();
 });
-
-// restartGame2.addEventListener("click", () => {
-//     checkIfGameIsRunning = false;
-//     startGame();
-// });
-// restartGame.addEventListener("click", () => {
-//     checkIfGameIsRunning = false;
-//     startGame();
-// });
-  
 
 window.addEventListener("load", cargaInicial);
 
